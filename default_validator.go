3feba41eba2cfa42a9ae1049104c7433ed6c7f95
hanser2015 <1019850453@qq.com>
2018-07-09 18:43:14 +0800

    init

---
+++ default_validator.go
@@ -0,0 +1,31 @@
+// Copyright 2017 Manu Martinez-Almeida.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package binding
+
+import (
+	"reflect"
+
+	"github.com/asaskevich/govalidator"
+)
+
+type defaultValidator struct {
+}
+
+var _ StructValidator = &defaultValidator{}
+
+// ValidateStruct receives any kind of type, but only performed struct or pointer to struct type.
+func (v *defaultValidator) ValidateStruct(obj interface{}) error {
+	value := reflect.ValueOf(obj)
+	valueType := value.Kind()
+	if valueType == reflect.Ptr {
+		valueType = value.Elem().Kind()
+	}
+	if valueType == reflect.Struct {
+		if _, err := govalidator.ValidateStruct(obj); err != nil {
+			return err
+		}
+	}
+	return nil
+}
